9ad2c3675d6c361546b1311dc981761d89a69908
myname21-meet <myname21@meet.mit.edu>
2019-07-29 07:16:40 -0400

    lo

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -8,14 +8,35 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(name, topic, rate):
+	article_object = Knowledge(
+		name = name,
+		topic = topic,
+		rate = rate)
+	session.add(article_object)
+	session.commit()
+add_article("Nathan", "music", 8)
+
+
+
 
 def query_all_articles():
-	pass
+	articles = session.query(
+      Knowledge).all()
+	return articles
 
-def query_article_by_topic():
-	pass
+
+print(query_all_articles())
+
+def query_article_by_topic(name):
+	ARTICLES = session.query(
+       Knowledge).filter_by(
+       name="Nathan_Goshen").first()
+	return ARTICLES
+
+
+print(query_article_by_topic("Nathan_Goshen"))
+
 
 def delete_article_by_topic():
 	pass
